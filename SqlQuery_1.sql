52c4afd3cb4833b9eaea559d20a4c70be8f5a9c9
Jonrork5 <133925439+Jonrork5@users.noreply.github.com>
2024-02-20 03:39:28 -0500

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,4 @@
+﻿SELECT Customers.CustomerName
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+WHERE Orders.OrderID = 10310
---
+++ SqlQuery_2.sql
@@ -0,0 +1,4 @@
+﻿SELECT Suppliers.Address
+FROM Suppliers
+INNER JOIN Products ON Suppliers.SupplierID = Products.SupplierID
+WHERE Products.ProductID = 40
